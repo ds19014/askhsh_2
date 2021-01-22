5f9340cceabd778d2c19f565afc855bef49662c3
ds19014 <ds19014@go.uop.gr>
2021-01-22 16:38:11 +0200

    Initial commit

---
+++ src/askhsh_2.java
@@ -0,0 +1,17 @@
+public class askhsh_2
+{
+    public static void main(String args[])
+    {
+        try {
+            if (args.length == 1) {
+                System.out.print("You are ");
+                System.out.print(args[0]);
+                System.out.println(" years old.");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.printf("My AM is %s", args[1]);
+        }
+    }
+}
